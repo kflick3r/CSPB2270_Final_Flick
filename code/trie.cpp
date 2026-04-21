bc032daed322523f104c774605b8b0c8983460eb
Kassidy Flick <kassidy.flick@colorado.edu>
2026-04-21 23:24:08 +0000

    Set up trie.cpp

---
+++ code/trie.cpp
@@ -0,0 +1,30 @@
+/* Note: refer to the header file (trie.h) for documentation of each method. */
+
+#include "trie.h"
+
+using namespace std;
+
+Trie::Trie() {
+    // Initialize empty root node
+    root = new TrieNode();
+}
+
+Trie::~Trie() {}
+
+
+void Trie::insert(string word) {
+    // 
+    
+}
+
+bool Trie::search(stringword) {
+    //
+
+    return false;
+}
+
+vector<string> Trie::autocomplete(string prefix) {
+    // 
+
+    return;
+}
